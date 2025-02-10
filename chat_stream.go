cd188147ac9f537720d68fd767f5c26f9a30827d
pptong5 <pptong5@iflytek.com>
2025-02-10 11:00:04 +0800

    feat:支持deepseek-r1, reasoning-content

--- chat_stream.go
+++ chat_stream.go
@@ -1,68 +1,69 @@
 package openai
 
 import (
-	"context"
-	"net/http"
+    "context"
+    "net/http"
 )
 
 type ChatCompletionStreamChoiceDelta struct {
-	Content      string        `json:"content,omitempty"`
-	Role         string        `json:"role,omitempty"`
-	FunctionCall *FunctionCall `json:"function_call,omitempty"`
-	ToolCalls    []ToolCall    `json:"tool_calls,omitempty"`
-	Refusal      string        `json:"refusal,omitempty"`
+    Content          string        `json:"content,omitempty"`
+    ReasoningContent string        `json:"reasoning_content,omitempty"`
+    Role             string        `json:"role,omitempty"`
+    FunctionCall     *FunctionCall `json:"function_call,omitempty"`
+    ToolCalls        []ToolCall    `json:"tool_calls,omitempty"`
+    Refusal          string        `json:"refusal,omitempty"`
 }
 
 type ChatCompletionStreamChoiceLogprobs struct {
-	Content []ChatCompletionTokenLogprob `json:"content,omitempty"`
-	Refusal []ChatCompletionTokenLogprob `json:"refusal,omitempty"`
+    Content []ChatCompletionTokenLogprob `json:"content,omitempty"`
+    Refusal []ChatCompletionTokenLogprob `json:"refusal,omitempty"`
 }
 
 type ChatCompletionTokenLogprob struct {
-	Token       string                                 `json:"token"`
-	Bytes       []int64                                `json:"bytes,omitempty"`
-	Logprob     float64                                `json:"logprob,omitempty"`
-	TopLogprobs []ChatCompletionTokenLogprobTopLogprob `json:"top_logprobs"`
+    Token       string                                 `json:"token"`
+    Bytes       []int64                                `json:"bytes,omitempty"`
+    Logprob     float64                                `json:"logprob,omitempty"`
+    TopLogprobs []ChatCompletionTokenLogprobTopLogprob `json:"top_logprobs"`
 }
 
 type ChatCompletionTokenLogprobTopLogprob struct {
-	Token   string  `json:"token"`
-	Bytes   []int64 `json:"bytes"`
-	Logprob float64 `json:"logprob"`
+    Token   string  `json:"token"`
+    Bytes   []int64 `json:"bytes"`
+    Logprob float64 `json:"logprob"`
 }
 
 type ChatCompletionStreamChoice struct {
-	Index                int                                 `json:"index"`
-	Delta                ChatCompletionStreamChoiceDelta     `json:"delta"`
-	Logprobs             *ChatCompletionStreamChoiceLogprobs `json:"logprobs,omitempty"`
-	FinishReason         FinishReason                        `json:"finish_reason"`
-	ContentFilterResults ContentFilterResults                `json:"content_filter_results,omitempty"`
+    Index                int                                 `json:"index"`
+    Delta                ChatCompletionStreamChoiceDelta     `json:"delta"`
+    Logprobs             *ChatCompletionStreamChoiceLogprobs `json:"logprobs,omitempty"`
+    FinishReason         FinishReason                        `json:"finish_reason"`
+    ContentFilterResults ContentFilterResults                `json:"content_filter_results,omitempty"`
 }
 
 type PromptFilterResult struct {
-	Index                int                  `json:"index"`
-	ContentFilterResults ContentFilterResults `json:"content_filter_results,omitempty"`
+    Index                int                  `json:"index"`
+    ContentFilterResults ContentFilterResults `json:"content_filter_results,omitempty"`
 }
 
 type ChatCompletionStreamResponse struct {
-	ID                  string                       `json:"id"`
-	Object              string                       `json:"object"`
-	Created             int64                        `json:"created"`
-	Model               string                       `json:"model"`
-	Choices             []ChatCompletionStreamChoice `json:"choices"`
-	SystemFingerprint   string                       `json:"system_fingerprint"`
-	PromptAnnotations   []PromptAnnotation           `json:"prompt_annotations,omitempty"`
-	PromptFilterResults []PromptFilterResult         `json:"prompt_filter_results,omitempty"`
-	// An optional field that will only be present when you set stream_options: {"include_usage": true} in your request.
-	// When present, it contains a null value except for the last chunk which contains the token usage statistics
-	// for the entire request.
-	Usage *Usage `json:"usage,omitempty"`
+    ID                  string                       `json:"id"`
+    Object              string                       `json:"object"`
+    Created             int64                        `json:"created"`
+    Model               string                       `json:"model"`
+    Choices             []ChatCompletionStreamChoice `json:"choices"`
+    SystemFingerprint   string                       `json:"system_fingerprint"`
+    PromptAnnotations   []PromptAnnotation           `json:"prompt_annotations,omitempty"`
+    PromptFilterResults []PromptFilterResult         `json:"prompt_filter_results,omitempty"`
+    // An optional field that will only be present when you set stream_options: {"include_usage": true} in your request.
+    // When present, it contains a null value except for the last chunk which contains the token usage statistics
+    // for the entire request.
+    Usage *Usage `json:"usage,omitempty"`
 }
 
 // ChatCompletionStream
 // Note: Perhaps it is more elegant to abstract Stream using generics.
 type ChatCompletionStream struct {
-	*streamReader[ChatCompletionStreamResponse]
+    *streamReader[ChatCompletionStreamResponse]
 }
 
 // CreateChatCompletionStream — API call to create a chat completion w/ streaming
@@ -70,37 +71,37 @@ type ChatCompletionStream struct {
 // sent as data-only server-sent events as they become available, with the
 // stream terminated by a data: [DONE] message.
 func (c *Client) CreateChatCompletionStream(
-	ctx context.Context,
-	request ChatCompletionRequest,
+        ctx context.Context,
+        request ChatCompletionRequest,
 ) (stream *ChatCompletionStream, err error) {
-	urlSuffix := chatCompletionsSuffix
-	if !checkEndpointSupportsModel(urlSuffix, request.Model) {
-		err = ErrChatCompletionInvalidModel
-		return
-	}
+    urlSuffix := chatCompletionsSuffix
+    if !checkEndpointSupportsModel(urlSuffix, request.Model) {
+        err = ErrChatCompletionInvalidModel
+        return
+    }
 
-	request.Stream = true
-	reasoningValidator := NewReasoningValidator()
-	if err = reasoningValidator.Validate(request); err != nil {
-		return
-	}
+    request.Stream = true
+    reasoningValidator := NewReasoningValidator()
+    if err = reasoningValidator.Validate(request); err != nil {
+        return
+    }
 
-	req, err := c.newRequest(
-		ctx,
-		http.MethodPost,
-		c.fullURL(urlSuffix, withModel(request.Model)),
-		withBody(request),
-	)
-	if err != nil {
-		return nil, err
-	}
+    req, err := c.newRequest(
+        ctx,
+        http.MethodPost,
+        c.fullURL(urlSuffix, withModel(request.Model)),
+        withBody(request),
+    )
+    if err != nil {
+        return nil, err
+    }
 
-	resp, err := sendRequestStream[ChatCompletionStreamResponse](c, req)
-	if err != nil {
-		return
-	}
-	stream = &ChatCompletionStream{
-		streamReader: resp,
-	}
-	return
+    resp, err := sendRequestStream[ChatCompletionStreamResponse](c, req)
+    if err != nil {
+        return
+    }
+    stream = &ChatCompletionStream{
+        streamReader: resp,
+    }
+    return
 }
